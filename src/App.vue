b88f131f0bbd7a0d9f8ef0bdce1e6dd613b24075
Andrew Sai <andrii.saienko@uzhnu.edu.ua>
2024-11-12 15:28:19 +0200

    initial bootstrap

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const data = ref(null);
+
+async function makeACall() {
+  const res = await fetch("http://localhost/api/");
+  data.value = await res.json();
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <p>Example response:</p>
+  <p>{{ data }}</p>
+  <button @click="makeACall">Make a Call</button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
